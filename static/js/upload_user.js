eee77069e458b9f1c4d63765a8c65b7c5117457b
etharali12345 <mayadaethar@gmail.com>
2025-11-29 17:49:37 +0300

    put uploaded data on js rather than on html& add scrolling

---
+++ static/js/upload_user.js
@@ -0,0 +1,100 @@
+const formMissing = document.getElementById("form-missing")
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-image");
+const imgView = document.getElementById("img-view");
+const errorMessage = document.getElementById("errorMessage");
+
+const resultColumn = document.getElementById("result-column");
+const circle = document.querySelector('.percent_circle');
+
+const percentHead = document.querySelector('.percent-head');
+const resultImage = document.getElementById("result-image");
+const resultPercent = document.getElementById("result-percent");
+const resultName = document.getElementById("resutl-name");
+const resultAge = document.getElementById("result-age");
+const resultSex = document.getElementById("result-sex");
+const resultCondition = document.getElementById("result-condition");
+const resultDateOfFounding = document.getElementById("result-dateOfFounding");
+const resultEntity = document.getElementById("result-findingEntity");
+const resultPlace = document.getElementById("result-place");
+const resultContact = document.getElementById("result-contact");
+
+//result data (to be replaced with server response)
+// if the sent form is not valid, the server will not respond with valid false and message 
+let data = {
+    valid: true,
+    match: true,
+    image_path: 'static/uploads/hostage1.jpg',
+    percent: 70,
+    name: "احمد محمد احمد",
+    age: 35,
+    sex: "ذكر",
+    condition: "سليم",
+    dateOfFounding: "2025/12/12",   
+    findingEntity: "منظمة الهلال الاحمر فرع الشمالية",
+    location: "الشمالية - وادي حلفا",
+    contact: "002496564646",
+    contact2: "0024965465656"
+};
+
+inputFile.addEventListener('change', function (){
+    let img = inputFile.files[0];
+    let imgLink = URL.createObjectURL(img);
+    imgView.style.backgroundImage = `url(${imgLink})`
+    imgView.textContent = "";   
+});
+
+formMissing.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    if(!formMissing.checkValidity()){
+        event.stopPropagation();
+        errorMessage.hidden = false;
+    } else {
+        errorMessage.hidden = true;
+        const formData = new FormData(formMissing);
+        const response = await fetch('/upload_missing', {
+            method: 'POST',
+            body: formData
+        });
+        //change data2 to data1
+        const data2 = await response.json();   
+
+        if(data.valid && data.match){
+            showResult(data);
+        }     
+    }
+});
+
+function showResult(data){
+    resultColumn.hidden = false;
+    const offset = 25; 
+    let scrollTop = resultColumn.getBoundingClientRect().top + window.scrollY;
+
+    if (window.innerWidth > 700) {
+        scrollTop -= offset;
+    }
+
+    window.scrollTo({
+        top: scrollTop,
+        behavior: "smooth"
+    });
+
+    percentHead.textContent = data.percent + "%";
+    resultImage.style.backgroundImage = `url(${data.image_path})`;
+    resultName.textContent = data.name;
+    resultAge.textContent = data.age;
+    resultSex.textContent = data.sex;
+    resultCondition.textContent = data.condition;
+    resultDateOfFounding.textContent = data.dateOfFounding;
+    resultEntity.textContent = data.findingEntity;
+    resultPlace.textContent = data.location;
+    resultContact.textContent = data.contact + " او " + data.contact2;
+
+    resultPercent.textContent = data.percent + "%";
+    let percent_counter = 0;
+    const interval = setInterval(() => {
+        percent_counter++;
+        circle.style.setProperty('--percent', percent_counter);
+        if (percent_counter >= data.percent) clearInterval(interval);
+    }, 20);
+}
